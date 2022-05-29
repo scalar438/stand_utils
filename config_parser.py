bdf45030e2a9fa11b44b35f51e3527551ff3af01
Alexander Larin <scalar438@hotmail.com>
2022-05-29 11:26:12 +0300

    Rename the login_url to auth_url

--- config_parser.py
+++ config_parser.py
@@ -3,10 +3,10 @@ import json
 class Config:
     def __init__(self, file_name = None):
         if file_name is None:
-            file_name = "config.json"
-        with open("config.json") as f:
+            file_name = "stand_utils/config.json"
+        with open(file_name) as f:
             params = json.load(f)
         self.login = params["login"]
         self.password = params["password"]
-        self.login_url =params["login_url"]
+        self.auth_url =params["auth_url"]
         self.schema_name = params["schema_name"]
